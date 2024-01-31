afcba00f8a3062ecbd70c62ce6f83f156279c8ba
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-01-31 23:51:13 +0530

    Update 26 Rehashing.cpp

--- 26 Rehashing.cpp
+++ 26 Rehashing.cpp
@@ -1,144 +1,156 @@
-/*A program to demonstrate rehashing*/
-#include<iostream>
+/*A program to implement rehashing*/
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int A[100],h[100],M,nv=0;
-int ele,idx,hkey,i;
-double u;
+class Lprobing {
+private:
+    vector<int*> h;
+    int M, nv = 0;
+    double u;
 
-class Lprobing{
 public:
-    void insert1(int);
-    void delete1(int);
-    void display(int);
-    void search1(int);
+    Lprobing(int M, double u) : M(M), u(u) {
+        h.resize(M, nullptr);
+    }
+
+    void insert1();
+    void delete1();
+    void display();
+    void search1();
     void rehashing();
-    void Lprobe(int x);
+    void Lprobe(int ele);
 };
 
-void Lprobing :: Lprobe(int M){
-    idx = ele % M;
-    if(h[idx] == NULL)
-        h[idx] = ele;
-    else{
-        for(i=0;i<M;i++){
-            hkey = (idx + i) % M;
-            if(h[hkey] == NULL){
-                h[hkey] = ele;
-                break;
+void Lprobing::Lprobe(int ele) {
+    int idx = ele % M;
+    if (h[idx] == nullptr) {
+        h[idx] = new int(ele);
+    } else {
+        for (int i = 0; i < M; i++) {
+            int hkey = (idx + i) % M;
+            if (h[hkey] == nullptr) {
+                h[hkey] = new int(ele);
+                return;
             }
         }
-        if(i==M)
-            cout<<"No space for Extra element";
+        cout << "No space for Extra element" << endl;
     }
 }
 
-void Lprobing :: insert1(int M){
-    cout<<"Enter element to insert: ";
-    cin>>ele;
+void Lprobing::insert1() {
+    cout << "Enter element to insert: ";
+    int ele;
+    cin >> ele;
     nv++;
-    cout<<nv/u<<endl;
-    if((nv/u)<0.75){
-        Lprobe(M);
-    }else{
-        for(i=0;i<M;i++)
-            A[i]=h[i];
+    cout << (double)nv / u << endl;
+    if ((double)nv / u < 0.75) {
+        Lprobe(ele);
+    } else {
         rehashing();
+        Lprobe(ele);
     }
 }
 
-void Lprobing ::rehashing(){
-     int x;
-     M = 2*M;
-     for(int i=0;i<M;i++)
-        h[i]=0;
-     for(i=0;i<M;i++){
-         x = A[i] % M;
-         if(h[x]== NULL)
-            h[x] = A[i];
-         else{
-            for(int j=0;j<M;j++){
-                hkey = (x + j) % M;
-                if(h[hkey] == NULL){
-                    h[hkey] = A[i];
-                    break;
-                }
-            }
-         }
-     }
-    u = 2*u;
-    if((nv/u)<0.75)
-        Lprobe(M);
-}
-
-void Lprobing ::display(int M){
-    cout<<"\tHASH TABLE\t"<<endl;
-    cout<<"Indices\t\tElements"<<endl;
-    for(i=0;i<M;i++)
-        cout<<i<<"\t\t"<<h[i]<<endl;
-}
-
-void Lprobing ::delete1(int M){
-    cout<<"Enter element to delete: ";
-    cin>>ele;
-    idx = ele % M;
-    if(h[idx] == ele)
-        h[idx] = NULL;
-    else{
-        for(i=0;i<M;i++){
-            hkey = (idx + i)%M;
-            if(h[hkey] == ele){
-                h[hkey]=NULL;
-                break;
-            }
-        }
-    }
-    cout<<"After deleting "<<ele<<endl;
-    cout<<"Elements are: "<<endl;
-    display(M);
-}
-
-void Lprobing :: search1(int M){
-    cout<<"Enter element to search: ";
-    cin>>ele;
-    idx = ele % M;
-    if(h[idx] == ele)
-        cout<<"Element "<<ele<<" found at index "<<idx<<endl;
-    else{
-        for(i=0;i<M;i++){
-            hkey = (idx + i)%M;
-            if(h[hkey] == ele){
-                cout<<"Element "<<ele<<" found at index "<<hkey<<endl;
-                break;
-            }
+void Lprobing::rehashing() {
+    vector<int*> old_h = h;
+    M *= 2;
+    h.resize(M, nullptr);
+    u *= 2;
+    nv = 0;
+    for (int* ele : old_h) {
+        if (ele != nullptr) {
+            Lprobe(*ele);
+            delete ele;
         }
     }
 }
 
-int main(){
+void Lprobing::display() {
+    cout << "\tHASH TABLE\t" << endl;
+    cout << "Indices\t\tElements" << endl;
+    for (int i = 0; i < M; i++) {
+        cout << i << "\t\t";
+        if (h[i] != nullptr)
+            cout << *h[i];
+        else
+            cout << "NULL";
+        cout << endl;
+    }
+}
+
+void Lprobing::delete1() {
+    cout << "Enter element to delete: ";
+    int ele;
+    cin >> ele;
+    int idx = ele % M;
+    if (h[idx] != nullptr && *h[idx] == ele) {
+        delete h[idx];
+        h[idx] = nullptr;
+    } else {
+        for (int i = 0; i < M; i++) {
+            int hkey = (idx + i) % M;
+            if (h[hkey] != nullptr && *h[hkey] == ele) {
+                delete h[hkey];
+                h[hkey] = nullptr;
+                break;
+            }
+        }
+    }
+    cout << "After deleting " << ele << endl;
+    cout << "Elements are: " << endl;
+    display();
+}
+
+void Lprobing::search1() {
+    cout << "Enter element to search: ";
+    int ele;
+    cin >> ele;
+    int idx = ele % M;
+    if (h[idx] != nullptr && *h[idx] == ele)
+        cout << "Element " << ele << " found at index " << idx << endl;
+    else {
+        for (int i = 0; i < M; i++) {
+            int hkey = (idx + i) % M;
+            if (h[hkey] != nullptr && *h[hkey] == ele) {
+                cout << "Element " << ele << " found at index " << hkey << endl;
+                return;
+            }
+        }
+        cout << "Element " << ele << " not found" << endl;
+    }
+}
+
+int main() {
     int ch;
-    Lprobing L;
-    cout<<"Enter table size: ";
-    cin>>M;
-    cout<<"Enter load factor value: ";
-    cin>>u;
-    cout<<"MENU \n1.Insert()\n2.Delete()\n3.Search()\n4.Display()\n5.Exit"<<endl;
-    do{
-        cout<<"Enter your choice: ";
-        cin>>ch;
-        switch(ch){
-            case 1 : L.insert1(M);
-                break;
-            case 2 : L.delete1(M);
-                break;
-            case 3 : L.search1(M);
-                break;
-            case 4 : L.display(M);
-                break;
-            case 5 :
-                return 0;
-            default : cout<<"Choice out of Range!!!"<<endl;
+    cout << "Enter table size: ";
+    int M;
+    cin >> M;
+    cout << "Enter load factor value: ";
+    double u;
+    cin >> u;
+    Lprobing L(M, u);
+    cout << "MENU \n1.Insert()\n2.Delete()\n3.Search()\n4.Display()\n5.Exit" << endl;
+    do {
+        cout << "Enter your choice: ";
+        cin >> ch;
+        switch (ch) {
+        case 1:
+            L.insert1();
+            break;
+        case 2:
+            L.delete1();
+            break;
+        case 3:
+            L.search1();
+            break;
+        case 4:
+            L.display();
+            break;
+        case 5:
+            return 0;
+        default:
+            cout << "Choice out of Range!!!" << endl;
         }
-    }while(1);
+    } while (1);
 }
-
