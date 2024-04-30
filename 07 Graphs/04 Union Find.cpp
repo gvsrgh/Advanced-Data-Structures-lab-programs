9b08906939bdc31b0fb93a99fbefba82dd4e1a6d
gvsrgh <148122487+gvsrgh@users.noreply.github.com>
2024-04-30 06:25:57 +0530

    Update 04 Union Find.cpp

--- 07 Graphs/04 Union Find.cpp
+++ 07 Graphs/04 Union Find.cpp
@@ -1,64 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Edge {
+class UnionFind {
+    vector<int> parent;
+
 public:
-    int src, dest;
-};
-
-class Graph {
-public:
-    int V, E;
-    Edge* edge;
-};
-
-Graph* createGraph(int V, int E) {
-    Graph* graph = new Graph();
-    graph->V = V;
-    graph->E = E;
-    graph->edge = new Edge[graph->E * sizeof(Edge)];
-    return graph;
-}
-
-int find(int parent[], int i) {
-    if (parent[i] == i)
-        return i;
-    return find(parent, parent[i]);
-}
-
-void Union(int parent[], int x, int y) {
-    parent[x] = y;
-}
-
-int isCycle(Graph* graph) {
-    int* parent = new int[graph->V];
-    for(int i = 0; i < graph->V; i++)
-        parent[i] = i;
-    for(int i = 0; i < graph->E; ++i) {
-        int x = find(parent, graph->edge[i].src);
-        int y = find(parent, graph->edge[i].dest);
-        if (x == y)
-            return 1;
-        Union(parent, x, y);
+    UnionFind(int n) {
+        parent.resize(n);
+        for (int i = 0; i < n; ++i)
+            parent[i] = i;
     }
-    return 0;
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void Union(int x, int y) {
+        parent[x] = y;
+    }
+};
+
+bool hasCycle(const vector<pair<int, int>>& edges, int n) {
+    UnionFind uf(n);
+
+    for (const auto& edge : edges) {
+        int x = uf.find(edge.first);
+        int y = uf.find(edge.second);
+
+        if (x == y)
+            return true;
+
+        uf.Union(x, y);
+    }
+    return false;
 }
 
 int main() {
-    int V, E;
+    int n, m;
     cout << "Enter the number of vertices and edges: ";
-    cin >> V >> E;
-    Graph* graph = createGraph(V, E);
-    cout << "Enter " << E << " edges (Source, Destination): " << endl;
-    for(int i = 0; i < E; i++) {
-        int src, dest;
-        cin >> src >> dest;
-        graph->edge[i].src = src;
-        graph->edge[i].dest = dest;
+    cin >> n >> m;
+
+    vector<pair<int, int>> edges(m);
+    cout << "Enter the edges (Source, Destination):" << endl;
+    for (int i = 0; i < m; ++i) {
+        cin >> edges[i].first >> edges[i].second;
     }
-    if (isCycle(graph))
-        cout << "Graph contains cycle"<<endl;
+
+    if (hasCycle(edges, n))
+        cout << "Graph contains cycle." << endl;
     else
-        cout << "Graph does not contain cycle"<<endl;
+        cout << "Graph does not contain cycle." << endl;
+
     return 0;
 }
